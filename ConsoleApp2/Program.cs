9fe23878f382be5467172971aa31b4314885e38c
Zlata Semenkova <zlatasemenkova@outlook.com>
2023-11-09 14:45:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    public static object Else { get; private set; }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 4;
+        int num4 = 8;
+        int num5 = 3;
+        Console.WriteLine(num1);
+        //5= Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine(num2);
+        //6= Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine(num3);
+        //4= Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine(num4);
+        //8= Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine(num5);
+        //3= Convert.ToInt32( Console.ReadLine() );
+
+        //int result = 5 + 6 + 4 + 8 + 3 = 26 / 5 ;
+        Console.WriteLine("$Result⁚ { 5,2 }");
+
+        Console.WriteLine("Enter mark⁚ ");
+        int mark = Convert.ToInt32(Console.ReadLine()); 
+        String result = string.Empty;
+
+        if (mark == 4 || mark == 12) ;
+        {
+            if (mark == 12) ;
+            result = " Rerfect "; 
+        }
+       // else
+        {
+            result = "ok ";
+        }
+        Console.WriteLine("result");
+    {
+        //else
+        }
+Console.WriteLine("Inkorrect mark");
+        
+
+
+Number = 22; 
+int result  = 22 * 3;
+Console.WriteLine("$Result⁚ {66 };
+
+
+
+
+
